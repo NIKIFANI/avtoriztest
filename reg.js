67836232e1b25604c5e0804c881f109561266225
losik <losik@LAPTOP-LGS34R0H>
2022-03-19 18:08:21 +0300

    avtorizireg

--- reg.js
+++ reg.js
@@ -1,20 +1,24 @@
 let username;
 let password;
+let fio;
+let email;
 
 document.querySelector('#j-submit').onclick = function (event) {
     event.preventDefault();
     username = document.querySelector('#username').value;
     password = document.querySelector('#password').value;
+    fio = document.querySelector('#fio').value;
+    email = document.querySelector('#email').value;
     let xhttp = new XMLHttpRequest();
-    let url = new URL('http://localhost:8080/login');
-    url.searchParams.set(username, password);
+    let url = new URL('http://localhost:8080/registration');
+    url.searchParams.set(username, password, fio, email);
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             myFunction(this.response)
         }
     }
 
-    xhttp.open('POST', "http://localhost:8085/login", true);
+    xhttp.open('POST', "http://localhost:8085/registration", true);
     xhttp.send();
 
 
@@ -22,6 +26,3 @@ document.querySelector('#j-submit').onclick = function (event) {
         console.log(data);
     }
 }
-
-
-
